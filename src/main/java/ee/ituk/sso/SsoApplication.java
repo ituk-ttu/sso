4e60042b2f6714e4759f45b5420cd3462f82ea7c
Liine Kasak <liine.kasak@gmail.com>
2019-02-02 14:56:09 +0200

    initial commit

---
+++ src/main/java/ee/ituk/sso/SsoApplication.java
@@ -0,0 +1,17 @@
+package ee.ituk.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@SpringBootApplication
+@EnableResourceServer
+public class SsoApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsoApplication.class, args);
+	}
+
+}
+
